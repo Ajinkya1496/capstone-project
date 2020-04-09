d25233a3e70d2b5cda934d352cd21c997d00060a
apple <apple@apples-MacBook-Pro.local>
2020-04-09 08:28:41 +0530

    changes/validations

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 let  divs = [];
 let algorithm; let array; let number;
-function submitData() {
-    // let size = document.getElementById("size").value;
-    let data = document.getElementById("data").value;
+
+function dataType() {
+    let dataType = document.getElementById("data-type").value;
+    let dataElem = document.getElementById("data");
+    let sizeElem = document.getElementById("size");
+    switch(dataType) {
+        case 'inputData':
+            dataElem.removeAttribute("hidden");
+            dataElem.setAttribute("required","true");
+
+            sizeElem.setAttribute("hidden","true");
+            sizeElem.removeAttribute("required");
+            break;
+        case 'generateData':
+            sizeElem.removeAttribute("hidden");
+            sizeElem.setAttribute("required","true");
+
+            dataElem.setAttribute("hidden","true");
+            dataElem.removeAttribute("required");
+            break;
+    }
+}
+
+function generateDataSet(size) {
+    let elements = [];
+    for (var i = 0; i < size; i++) {
+        let num = Math.floor(Math.random() * 100) + 1;
+        if(elements.indexOf(num) === -1) elements.push(num);
+    }
+    return elements;
+}
+
+function submitData(event) {
+    event.preventDefault();
+    let dataType = document.getElementById("data-type").value;
+    switch(dataType) {
+        case 'inputData':
+            let data = document.getElementById("data").value;
+            array = data.split(",");
+            break;
+        case 'generateData':
+            let size = document.getElementById("size").value;
+            array = generateDataSet(size);
+            break;
+    }
+
     algorithm = document.getElementById("algorithm").value;
 
-    array = data.split(",");
     if(algorithm === "linear" || algorithm === "binary") {
         array.sort(function(a,b) {
             return a-b;
@@ -20,7 +62,8 @@ function submitData() {
     // console.log(divs)
 }
 
-function run() {
+function run(event) {
+    event.preventDefault();
     if(algorithm === "linear") {
         for(let i=0;i<divs.length;i++) {
             divs.item(i).classList.remove("found");
@@ -151,6 +194,10 @@ function generateElements(array) {
         }
         showData.style.display = "inline-flex";
         return showData.getElementsByTagName("div");
+}
+
+function resetVisualiser() {
+    window.location.reload();
 }
 
 function delay() {  
